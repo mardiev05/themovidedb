30f360c9d06bc13840f379d054d4e6c614438540
mardiev.dev <mardiev2125@gmail.com>
2022-04-10 14:11:31 -0500

    review section has been completed in JavaScript

--- details.js
+++ details.js
@@ -3,16 +3,19 @@ let imgUrl = "https://image.tmdb.org/t/p/w500/";
 let about = document.querySelector(".about");
 let bgImg = document.querySelector(".bgImg");
 
-(async() => {
-        let getItem = localStorage.getItem("id");
+let getItem = localStorage.getItem("id");
 
-        let res = await fetch(
-            "https://api.themoviedb.org/3/movie/" +
-            getItem +
-            "?api_key=2cf8f0300d87595c63b0fe796e506522&language=en-US"
-        );
+let movie = "https://api.themoviedb.org/3/movie/" + getItem + "?api_key=2cf8f0300d87595c63b0fe796e506522&language=en-US";
+let external_ids = "https://api.themoviedb.org/3/movie/" + getItem + "/external_ids?api_key=2cf8f0300d87595c63b0fe796e506522";
+let credits = "https://api.themoviedb.org/3/movie/" + getItem + "/credits?api_key=2cf8f0300d87595c63b0fe796e506522&language=en-US";
+let reviews = "https://api.themoviedb.org/3/movie/" + getItem + "/reviews?api_key=2cf8f0300d87595c63b0fe796e506522&language=en-US&page=1";
+
+
+
+(async() => {
+
+        let res = await fetch(`${movie}`);
         let detailes = await res.json();
-        console.log(detailes);
 
         // for (let detailes of data) {
 
@@ -21,9 +24,8 @@ let bgImg = document.querySelector(".bgImg");
 
         let ab = `
         <div class="about__img">
-            <img class="about__main-img" src="${imgUrl}${
-    detailes.poster_path
-  }" alt="${detailes.title}" />
+            <img class="about__main-img" src="${imgUrl}${detailes.poster_path
+        }" alt="${detailes.title}" />
             <div class="about__img-detail">
                 <img src="https://www.themoviedb.org/t/p/original/peURlLlr8jggOwK53fJ5wdQl05y.jpg" alt="" />
                 <p>
@@ -35,18 +37,17 @@ let bgImg = document.querySelector(".bgImg");
 
         <div class="about__content">
             <h1 class="about__content-title">
-                <a href="#">${detailes.original_title} ${
-    detailes.release_date
-  }</a>
+                <a href="#">${detailes.original_title} ${detailes.release_date
+        }</a>
             </h1>
             <ul class="about__dates">
                 <li class="about__date">${detailes.release_date} (US)</li>
                 <li>
                     ${detailes.genres.map((item) => {
-                      return `<a class="about__type" href="#">
-                          ${item.name}
+            return `<a class="about__type" href="#">
+                            ${item.name}
                         </a>`;
-                    })}
+        })}
                 </li>
                 <li class="about__hour">${detailes.runtime}</li>
             </ul>
@@ -66,26 +67,82 @@ let bgImg = document.querySelector(".bgImg");
                 ${detailes.overview}
                 </p>
                 <div class="overvieww__row">
-                    ${detailes.production_companies.map(
-                      (item) =>
-                        `<div class="overview__card">
+                    ${detailes.production_companies.map((item) => {
+            return `<div class="overview__card">
                             <div class="overview__img">
                                 <img src="${imgUrl}${item.logo_path}" style=" width:50px">
                             </div>
                             <p class="overview__pos">${item.name}</p>
-                        </div>`
-                    )}
+                        </div>`;
+        })}
                 </div>
             </div>
         </div>
         `;
-  about.innerHTML = ab;
+    about.innerHTML = ab;
 })();
+
+(async () => {
+    let res = await fetch(`${credits}`);
+    let data = await res.json();
+    let section = document.querySelector(".content__cast-row");
+
+    for (let item of data.cast) {
+        let card = `
+            <div class="cast__row-card">
+                <img src="${imgUrl}${item.profile_path} " alt="" />
+                 <div class="cast__card-texts">
+                    <h5 class="cast__name">${item.name}</h5>
+                    <p class="cast__text">${item.character}</p>
+                </div>
+            </div >
+        `;
+        section.innerHTML += card;
+    }
+
+
+
+})();
+
+(async () => {
+    try {
+        let res = await fetch(`${reviews}`);
+        let data = await res.json();
+        console.log(data);
+        const date = new Date(data.results[0].updated_at);
+        let review = `
+                
+                <div class="social__content-review">
+                    <div class="social__content-img">
+                        <img src="${data.results[0].author_details.avatar_path}" alt="" />
+                    </div>
+                    <div class="social__content">
+                        <div class="social__content-rating">
+                            <a class="social__content-title" href="#">A review by ${data.results[0].author}</a>
+                            <span class="rounded__rating">
+                                <i class="fa-solid fa-star"></i> ${data.results[0].author_details.rating}.0
+                            </span>
+                        </div>
+                        <p class="written">
+                            Written by <b class="bold">${data.results[0].author_details.username}</b> on ${date.toDateString()}
+                        </p>
+                        <p class="text">
+                            ${data.results[0].content}
+                        </p>
+                    </div>
+                </div>
+        `;
+        document.querySelector(".social__review-card").innerHTML = review;
+    } catch (err) {
+        console.log(err);
+    }
+})()
+
 
 // header logo
 let headerLogo = document.querySelector(".header__links-logo");
 
 headerLogo.addEventListener("click", (e) => {
-  e.preventDefault();
-  window.location.assign("http://127.0.0.1:5500/main.html");
+    e.preventDefault();
+    window.location.assign("http://127.0.0.1:5500/main.html");
 });
